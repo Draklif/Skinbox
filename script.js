e6e1655c7fbb712799debaf4ed3ad54d597ebabd
Danklif <josemanrenteria@gmail.com>
2023-11-15 00:33:57 -0500

    151123-01 Tarjetas de momento

--- script.js
+++ script.js
@@ -3,29 +3,29 @@ import * as THREE from 'three';
 console.log(window.outerWidth);
 console.log(window.outerHeight);
 if (window.outerWidth >= 1024) {
-    const canvas = document.getElementById('canvas')
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    // const canvas = document.getElementById('canvas')
+    // const scene = new THREE.Scene();
+    // const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xc0342f }))
+    // const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xc0342f }))
 
-    scene.background = new THREE.Color(0xf4f2ec)
-    scene.add(cube)
+    // scene.background = new THREE.Color(0xf4f2ec)
+    // scene.add(cube)
 
-    camera.position.z = 5;  
+    // camera.position.z = 5;  
 
-    const renderer = new THREE.WebGLRenderer({ canvas: canvas });
-    renderer.setSize( 500, 500 );
-    renderer.render(scene, camera)
+    // const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+    // renderer.setSize( 500, 500 );
+    // renderer.render(scene, camera)
 
-    function animate() {
-        requestAnimationFrame(animate);
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+    // function animate() {
+    //     requestAnimationFrame(animate);
+    //     cube.rotation.x += 0.01;
+    //     cube.rotation.y += 0.01;
 
-        renderer.render(scene, camera)
-    }
-    animate()
+    //     renderer.render(scene, camera)
+    // }
+    // animate()
 
     TweenMax.from('.navbar', 1, {
         delay: 1,
@@ -46,14 +46,14 @@ if (window.outerWidth >= 1024) {
         ease: Circ.easeInOut
     })
 
-    TweenMax.from(".text h2", 1, {
+    TweenMax.from(".main .skinbox", 1, {
         delay: 3.3,
         opacity: 0,
         y: 40,
         ease: Circ.easeInOut
     })
 
-    TweenMax.from(".text .para", 1.2, {
+    TweenMax.from(".main .para", 1.2, {
         delay: 3.9,
         opacity: 0,
         y: 40,
